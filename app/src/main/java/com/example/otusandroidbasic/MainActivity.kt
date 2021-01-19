feeeeff82b39f385f3db547c1346014a8176ee25
MUlt1mate <multimate.abs@gmail.com>
2021-01-19 21:43:56 +0300

    movie list

--- app/src/main/java/com/example/otusandroidbasic/MainActivity.kt
+++ app/src/main/java/com/example/otusandroidbasic/MainActivity.kt
@@ -1,25 +1,78 @@
 package com.example.otusandroidbasic
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val tag = "MainActivity"
+
+    companion object {
+        const val SELECTED_TITLE = "SELECTED_TITLE"
+        const val SELECTED_TITLE_COLOR = "#B33C3C"
+        const val DEFAULT_TITLE_COLOR = "#323232"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(tag, "created")
 
-        findViewById<View>(R.id.movieButton1).setOnClickListener {
+        fun setTextColor(elementId: String, color: String) {
+            val id = this.resources.getIdentifier(elementId, "id", this.packageName)
+            findViewById<TextView>(id)?.setTextColor(Color.parseColor(color))
+        }
+
+        intent.getStringExtra(SELECTED_TITLE)?.let {
+            Log.i(tag, "found selected title: $it")
+            setTextColor(it, SELECTED_TITLE_COLOR)
+        }
+
+        fun clickDetail(data: MovieData, titleId: String) {
+            Log.i(tag, "click on movie ${data.title}")
+            // снимаем выделение с предыдущего выбранного фильма, если был
+            intent.getStringExtra(SELECTED_TITLE)?.let {
+                Log.i(tag, "disable selected title: $it")
+                setTextColor(it, DEFAULT_TITLE_COLOR)
+            }
+            setTextColor(titleId, SELECTED_TITLE_COLOR)
+            intent.putExtra(SELECTED_TITLE, titleId)
             Intent(this, MovieDescription::class.java).apply {
-                val data = MovieData(
-                    "@string/darkKnightTitle",
-                    "@drawable/batman6_23s",
-                    "@string/darkKnightDescription"
-                )
                 putExtra(MovieDescription.MOVIE_DATA, data)
                 startActivity(this)
             }
+        }
+
+        findViewById<View>(R.id.movieButton1).setOnClickListener {
+            clickDetail(
+                MovieData(
+                    "@string/darkKnightTitle",
+                    "@drawable/batman6_23s",
+                    "@string/darkKnightDescription"
+                ), "@posterTitle1"
+            )
+        }
+        findViewById<View>(R.id.movieButton2).setOnClickListener {
+            clickDetail(
+                MovieData(
+                    "@string/quietPlaceTitle",
+                    "@drawable/quietplace2_10s",
+                    "@string/quietPlaceDescription"
+                ), "@posterTitle2"
+            )
+        }
+        findViewById<View>(R.id.movieButton3).setOnClickListener {
+            clickDetail(
+                MovieData(
+                    "@string/furious9Title",
+                    "@drawable/furious9_12s",
+                    "@string/furious9Description"
+                ), "@posterTitle3"
+            )
         }
     }
 }
--- app/src/main/java/com/example/otusandroidbasic/MovieDescription.kt
+++ app/src/main/java/com/example/otusandroidbasic/MovieDescription.kt
@@ -3,12 +3,15 @@ package com.example.otusandroidbasic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ShareActionProvider
 import android.widget.TextView
 
 class MovieDescription : AppCompatActivity() {
+    private val tag = "MovieDescription"
+
     companion object {
         const val MOVIE_DATA = "MOVIE_DATA"
     }
@@ -16,22 +19,22 @@ class MovieDescription : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_description)
+        Log.i(tag, "created")
 
         findViewById<View>(R.id.shareBtn).setOnClickListener {
+            Log.i(tag, "clicked share")
             val intent = Intent(this, ShareActionProvider::class.java)
             startActivity(intent)
         }
 
         intent.getParcelableExtra<MovieData>(MOVIE_DATA)?.let {
-            findViewById<TextView>(R.id.movieTitle).text = it.title
-            findViewById<TextView>(R.id.movieDescription).text = it.description
-            findViewById<ImageView>(R.id.detailsPoster).setImageResource(
-                this.resources.getIdentifier(
-                    it.img,
-                    "id",
-                    this.packageName
-                )
-            )
+            Log.i(tag, "received movie data ${it.title}")
+            fun getResourceById(name: String) =
+                this.resources.getIdentifier(name, "id", this.packageName)
+
+            findViewById<TextView>(R.id.movieTitle).setText(getResourceById(it.title))
+            findViewById<TextView>(R.id.movieDescription).setText(getResourceById(it.description))
+            findViewById<ImageView>(R.id.detailsPoster).setImageResource(getResourceById(it.img))
         }
     }
 }
